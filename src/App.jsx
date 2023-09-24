fc56b7d22132889a90ebd21495427fab452ef8e4
Vishesh <ranavisheshsingh242@gmail.com>
2023-09-24 22:19:18 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,47 @@
 import { useState,useEffect } from 'react';
 import {fetchDataFromApi} from "./utils/api";
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import { useSelector,useDispatch } from 'react-redux';
 import {getApiConfiguration} from "./store/homeSlice";
+import HeroBanner from './pages/home/heroBanner/HeroBanner';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from './pages/home/Home';
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import pageNotFound from './pages/404/pageNotFound';
 function App() {
   const dispatch=useDispatch();
   const {url}=useSelector((state)=>state.home);
   useEffect(()=>{
-    apiTesting();
+    fetchApiConfig();
   },[]);
-  const apiTesting=()=>{
-    fetchDataFromApi('/movie/popular')
+  const fetchApiConfig=()=>{
+    fetchDataFromApi('/configuration')
        .then((res)=>{
           console.log(res);
-          dispatch(getApiConfiguration(res));
+          const url={
+            backdrop:res.images.secure_base_url +"original",
+            poster:res.images.secure_base_url +"original",
+            profile:res.images.secure_base_url +"original",
+          }
+          dispatch(getApiConfiguration(url));
        })
   }
   
   return (
-    <>
-      APP
-      {url?.total_pages} 
-      
-    </>
-  )
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/:mediaType/:id" element={<Details/>}/>
+    <Route path="/search/:query" element={<SearchResult/>}/>
+    <Route path="/explore/:mediaType" element={<Explore/>}/>
+    <Route path="*" element={<pageNotFound/>}/>
+  </Routes>
+  <Footer/>
+  </BrowserRouter>)
 }
 
 export default App;
---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+
+const Trending = () => {
+  return (
+    <div>Trending</div>
+  )
+}
+
+export default Trending
